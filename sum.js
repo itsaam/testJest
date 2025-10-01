8d04d9afe0ae7b7c355662f3958132c6a0ca1a97
May <nospher4z@gmail.com>
2025-10-01 11:05:37 +0200

    test copilot review

--- sum.js
+++ sum.js
@@ -1,4 +1,9 @@
 function sum(a, b) {
+    if (!a) {
+        return 0; // Mauvaise pratique : change le comportement attendu
+    }
+    console.log("debug:", a, b); // console.log inutile
     return a + b;
 }
+
 module.exports = sum;
